0da3ddf2850138697c8fc87cafa9bc67a29613ba
coderdal <agircoder@gmail.com>
2023-09-24 16:43:37 +0300

    layout structure made & sidebar created

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,9 @@
+export default function SideBar() {
+    return (
+        <section className="w-64 bg-red-500 p-1.5">
+            <nav>
+                Left Sidebar
+            </nav>
+        </section>
+    );
+}
