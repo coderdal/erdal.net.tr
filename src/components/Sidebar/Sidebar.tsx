4ee9029857aec72fa778df458c15dc1b4caa5a3f
coderdal <agircoder@gmail.com>
2023-09-25 12:32:27 +0300

    sidebar version and info section added

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,15 +6,16 @@ import { GoCodeOfConduct } from "react-icons/go";
 import { TfiWrite } from "react-icons/tfi";
 import { RiShining2Line } from "react-icons/ri";
 import { HiOutlineMail } from "react-icons/hi";
-import { BiLogoLinkedin } from "react-icons/bi";
+import { BiLogoLinkedin, BiLogoTypescript } from "react-icons/bi";
 import { VscGithub } from "react-icons/vsc";
 import { FaMediumM } from "react-icons/fa";
-import { SiLeetcode } from "react-icons/si";
+import { SiLeetcode, SiTailwindcss } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
  const SideBar = () => {
     return (
         <section className="border-r bg-zinc-50 p-1.5 w-60 xl:w-72">
-            <nav>
+            <nav className="h-full flex flex-col">
                 <ProfileCard title="Software Developer" picture="/assets/images/pp.png">Muhammed ERDAL</ProfileCard>
                 <HorizontalLine />
                 <SidebarItem Icon={GoCodeOfConduct} href="/">Home</SidebarItem>
@@ -26,6 +27,11 @@ import { SiLeetcode } from "react-icons/si";
                 <SidebarItem Icon={VscGithub} href="https://github.com/coderdal" isExternal={true}>Github</SidebarItem>
                 <SidebarItem Icon={SiLeetcode} href="https://leetcode.com/coderdal/" isExternal={true}>Leetcode</SidebarItem>
                 <SidebarItem Icon={FaMediumM} href="https://medium.com/@coderdal" isExternal={true}>Medium</SidebarItem>
+
+                <div className="info mt-auto flex justify-between text-gray-600 text-sm select-none">
+                    <span>v1.0.0</span>
+                    <span className="flex items-center gap-1 text-xs"><TbBrandNextjs className="text-xl" />&<BiLogoTypescript className="text-xl" />&<SiTailwindcss className="text-xl" /></span>
+                </div>
             </nav>
         </section>
     );
