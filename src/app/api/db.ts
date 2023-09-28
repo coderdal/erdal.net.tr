2c46256b3e548ca2fbce821d92583cd5789c08f8
coderdal <agircoder@gmail.com>
2023-09-28 16:04:29 +0300

    debug db connection on live

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("db connection");
+    console.log(process.env.MONGODB_URI);
+    
     await mongoose.connect(process.env.MONGODB_URI || "");
   } catch (error) {
     console.error("An error occurred while database connection", error);
